56e7c1110e9fe0319abe02f3f6f1d7581fce86f3
marielapg <mpgonzales77@gmail.com>
2020-01-15 13:48:41 -0800

    api day 3 middleware

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -2,108 +2,124 @@ const express = require('express');
 
 const Hubs = require('./hubs-model.js');
 const Messages = require('../messages/messages-model.js');
+const checkFor = require('./checkForMiddleware.js');
 
 const router = express.Router();
+
+// write a middleware called checkFor that takes the name of a property and checks that the body has that property.
+// if the property is not there, respond with a status code 400 and an object like this { errorMessage: `required ${property}` }
+// where property is what we're validating. If we use it like this: checkFor('age') the message will read "required age".
+// use it on the POST and PUT to check that the body has the "name" property
+
+function check(prop) {
+  return function(req, res, next) {
+    if (req.body[prop]) {
+      next();
+    } else {
+      res.status(400).json({ errorMessage: `required ${prop}` });
+    }
+  };
+}
 
 // this only runs if the url has /api/hubs in it
 router.get('/', (req, res) => {
   Hubs.find(req.query)
-  .then(hubs => {
-    res.status(200).json(hubs);
-  })
-  .catch(error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hubs',
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the hubs',
+      });
     });
-  });
 });
 
 // /api/hubs/:id
 
 router.get('/:id', (req, res) => {
   Hubs.findById(req.params.id)
-  .then(hub => {
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'Hub not found' });
-    }
-  })
-  .catch(error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hub',
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: 'Hub not found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the hub',
+      });
     });
-  });
 });
 
-router.post('/', (req, res) => {
+router.post('/', checkFor('name'), uppercaser, (req, res) => {
   Hubs.add(req.body)
-  .then(hub => {
-    res.status(201).json(hub);
-  })
-  .catch(error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error adding the hub',
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error adding the hub',
+      });
     });
-  });
 });
 
 router.delete('/:id', (req, res) => {
   Hubs.remove(req.params.id)
-  .then(count => {
-    if (count > 0) {
-      res.status(200).json({ message: 'The hub has been nuked' });
-    } else {
-      res.status(404).json({ message: 'The hub could not be found' });
-    }
-  })
-  .catch(error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error removing the hub',
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: 'The hub has been nuked' });
+      } else {
+        res.status(404).json({ message: 'The hub could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error removing the hub',
+      });
     });
-  });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', uppercaser, (req, res) => {
   Hubs.update(req.params.id, req.body)
-  .then(hub => {
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'The hub could not be found' });
-    }
-  })
-  .catch(error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error updating the hub',
+    .then(hub => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: 'The hub could not be found' });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the hub',
+      });
     });
-  });
 });
 
 // add an endpoint that returns all the messages for a hub
 // this is a sub-route or sub-resource
 router.get('/:id/messages', (req, res) => {
   Hubs.findHubMessages(req.params.id)
-  .then(messages => {
-    res.status(200).json(messages);
-  })
-  .catch (error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error getting the messages for the hub',
+    .then(messages => {
+      res.status(200).json(messages);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error getting the messages for the hub',
+      });
     });
-  });
 });
 
 // add an endpoint for adding new message to a hub
@@ -111,16 +127,29 @@ router.post('/:id/messages', (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   Messages.add(messageInfo)
-  .then(message => {
-    res.status(210).json(message);
-  })
-  .catch(error => {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error getting the messages for the hub',
+    .then(message => {
+      res.status(210).json(message);
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error getting the messages for the hub',
+      });
     });
-  });
 });
 
+// TREST : trust but test.
+
+function uppercaser(req, res, next) {
+  if (typeof req.body.name === 'string') {
+    req.body.name = req.body.name.toUpperCase();
+    // req.name.age = 50;
+    // delete req.foo;
+
+    next();
+  } else {
+    res.status(400).json({ errorMessage: 'the name should be a string' });
+  }
+}
 module.exports = router;
---
+++ notes.md
@@ -0,0 +1,13 @@
+Types (how we got it)
+
+- built in: no need to install it, it's part of express.
+- third party: need to `npm install` it.
+  - helmet
+  - morgan
+  - express-validator
+- custom middleware: we build this one.
+
+Type (how we use it)
+
+- global: applies to all requests
+- local: applies to a set of routes (URLS)
